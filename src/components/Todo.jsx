31d39106c40de65b6984a73c53c3da9872d50036
manav1913 <69932908+manav1913@users.noreply.github.com>
2026-04-17 04:28:24 -0700

    Added add todo functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,20 +2,35 @@ import React, { useState } from 'react'
 
 const Todo = () => {
 
-    let todos = []
+    
 
-    const [todList, settodList] = useState(todos)
+    const [todList, seTtodList] = useState([])
+    const [input, setInput] = useState("")
 
+    const addTodo = () => {
+        if(input === "") return
+
+        setTodList([...todList, input])
+        setInput ("")
+    }
+
+    const deleteTodo = () => {
+        
+    }
 
 
   return (
 
     <>
     <div>
-      <input type="text" placeholder='Enter Todo...' />
-      <button>Add</button>
+      <input type="text"
+       placeholder='Enter Todo...'
+       value={input}
+       onChange={(e)=> setInput(e.target.value)}
+       />
+      <button onClick={addTodo}>Add</button>
       {todList.map((todo, index)=> <div key={index}>
-        <p>{todo}</p>
+        <p>{todo} <button onClick={deleteTodo}>Delete</button></p>
       </div>)}
     </div>
 
